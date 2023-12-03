1b67a59ada4839cb6e24ab67cbed7e2aa7f24631
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-03 05:54:30 +0000

    adds day 3 solution

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,6 +1,54 @@
-module Day03 where -- (day03) where
+module Day03 (day03) where
 
 import Common
+import Control.Monad (ap)
+import Data.Char (isDigit)
+import Data.HashMap.Strict qualified as M
+import Data.List (nub)
+import Data.Maybe (mapMaybe)
+
+data Schematic = Symbol Char | PartNumber Point2d Int deriving (Eq, Show)
 
 day03 :: AOCSolution
-day03 input = []
+day03 = map show . ap [part1, part2] . pure . parseInput
+
+part1 :: M.HashMap Point2d Schematic -> Int
+part1 i = sum n
+  where
+    s = M.keys $ M.filter isSymbol i
+    n = map snd $ nub $ concatMap (findAdjacentPartNumbers i) s
+
+part2 :: M.HashMap Point2d Schematic -> Int
+part2 i = sum $ map product n
+  where
+    s = M.keys $ M.filter isGear i
+    n = map2 snd $ filter ((== 2) . length) $ map (findAdjacentPartNumbers i) s
+
+findAdjacentPartNumbers :: M.HashMap Point2d Schematic -> Point2d -> [(Point2d, Int)]
+findAdjacentPartNumbers i p = map fromPartNumber $ nub $ mapMaybe (`M.lookup` i) n
+  where
+    n = point2dNeighboursDiags p
+
+parseInput :: String -> M.HashMap Point2d Schematic
+parseInput = M.fromList . concat . zipWith (\n s -> parseLine n (0, s)) [0 ..] . lines
+
+parseLine :: Int -> (Int, String) -> [(Point2d, Schematic)]
+parseLine _ (_, []) = []
+parseLine n (i, xss@(x : xs))
+  | isDigit x = [((i + i', n), PartNumber (i, n) $ read engine) | i' <- [0 .. ln - 1]] ++ parseLine n (i + ln, drop ln xss)
+  | x == '.' = parseLine n (i + 1, xs)
+  | otherwise = ((i, n), Symbol x) : parseLine n (i + 1, xs)
+  where
+    engine = takeWhile isDigit xss
+    ln = length engine
+
+fromPartNumber :: Schematic -> (Point2d, Int)
+fromPartNumber (PartNumber p n) = (p, n)
+
+isSymbol :: Schematic -> Bool
+isSymbol (Symbol _) = True
+isSymbol _ = False
+
+isGear :: Schematic -> Bool
+isGear (Symbol '*') = True
+isGear _ = False
--- test/Day03Spec.hs
+++ test/Day03Spec.hs
@@ -2,12 +2,22 @@ module Day03Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "467..114..\n\
+  \...*......\n\
+  \..35..633.\n\
+  \......#...\n\
+  \617*......\n\
+  \.....+.58.\n\
+  \..592.....\n\
+  \......755.\n\
+  \...$.*....\n\
+  \.664.598.."
 
 spec :: Spec
 spec = describe "Day 3" $ do
   it "Sample" $ do
-    day03 testInput `shouldBe` []
+    day03 testInput `shouldBe` ["4361", "467835"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +25,5 @@ spec = describe "Day 3" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day03 actualInput `shouldBe` []
+          day03 actualInput `shouldBe` ["498559", "72246648"]
       )
