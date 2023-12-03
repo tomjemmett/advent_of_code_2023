72bd1ebbeaa95add4049233db6d9d6453e5651eb
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-03 05:47:08 +0000

    refactor day 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,50 +1,44 @@
 module Day02 (day02) where
 
 import Common
-import Data.HashMap.Strict qualified as M
+import Control.Monad (ap)
 import Text.Parsec qualified as P
 
 day02 :: AOCSolution
-day02 input = show <$> ([part1, part2] <*> pure (parseInput input))
+day02 = map show . ap [part1, part2] . pure . parseInput
 
-part2 :: [(Int, [[(Int, String)]])] -> Int
-part2 = sum . map (power . maxEachColour)
+part1, part2 :: [(Int, [Int])] -> Int
+part1 = sum . map gameValid
   where
-    power :: M.HashMap String Int -> Int
-    power = product . map snd . M.toList
+    gameValid (gId, [r, g, b])
+      | r > 12 = 0
+      | g > 13 = 0
+      | b > 14 = 0
+      | otherwise = gId
+part2 = sum . map (product . snd)
 
-    maxEachColour :: (Int, [[(Int, String)]]) -> M.HashMap String Int
-    maxEachColour = foldr (uncurry (flip $ M.insertWith max)) m . concat . snd
-
-    m :: M.HashMap String Int
-    m = M.fromList [("red", 0), ("green", 0), ("blue", 0)]
-
-part1 :: [(Int, [[(Int, String)]])] -> Int
-part1 = foldr ((+) . gameValid) 0
-  where
-    gameValid :: (Int, [[(Int, String)]]) -> Int
-    gameValid (gId, grabs) = if all grabValid grabs then gId else 0
-
-    grabValid :: [(Int, String)] -> Bool
-    grabValid = all f
-
-    f :: (Int, String) -> Bool
-    f (i, colour) = case colour of
-      "red" -> i <= 12
-      "green" -> i <= 13
-      "blue" -> i <= 14
-
-parseInput :: String -> [(Int, [[(Int, String)]])]
-parseInput = map (parse' p id) . lines
+parseInput :: String -> [(Int, [Int])]
+parseInput = parse' (p `P.sepEndBy` P.newline) id
   where
     p = do
-      gId <- P.string "Game " *> number
-      P.string ": "
-      grabs <- pGrab `P.sepBy` P.string "; "
-      pure (gId, grabs)
-    pGrab = pCube `P.sepBy` P.string ", "
+      -- strip the id out between the text "Game " and ":"
+      gId <- P.between (P.string "Game ") (P.char ':') number
+      -- get all of the colour/values for the entire game
+      grabs <- pCube `P.sepBy` P.oneOf ",;"
+      -- return the game and the values, reducing the R/G/B values to the max
+      -- value for the current game
+      pure (gId, foldr f [0, 0, 0] grabs)
     pCube = do
-      n <- number
-      P.char ' '
+      -- each cube starts with a space, followed by a number, followed by a
+      -- space, but we only care about keeping the number
+      n <- P.between P.space P.space number
+      -- get the name of the colour, which will be made up of letters only
       c <- P.many P.letter
+      -- return the number and the colour
       pure (n, c)
+    -- take a number and a colour, and the current R/G/B values, and alter the
+    -- value for that colour if the new value is bigger
+    f (n, c) [r, g, b] = case c of
+      "red" -> [max r n, g, b]
+      "green" -> [r, max g n, b]
+      "blue" -> [r, g, max b n]
