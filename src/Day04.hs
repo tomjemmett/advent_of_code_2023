fbb99a0884ece3e3f0cfee2f5224ee9862e6bd5a
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-04 06:56:36 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,6 +1,41 @@
-module Day04 where -- (day04) where
+module Day04 (day04) where
 
 import Common
+import Control.Monad (ap)
+import Data.HashSet qualified as S
+import Text.Parsec qualified as P
 
 day04 :: AOCSolution
-day04 input = []
+day04 = map show . go . parseInput
+
+-- our fold function solves part 1 and 2 together. The accumulator that we use
+-- is a list containing the answers for p1/2 respectively, and a list which
+-- represents how many times each card should be represented (for part 2).
+
+-- for part 1, if the amount of winning numbers for a card is zero, do nothing
+-- otherwise take the winning numbers and 2 to the power of the predecessor of
+-- that value. e.g. 1 -> 2^0 = 1, 2 -> 2^1 = 2, 3 -> 2^2 = 4, 4 -> 2^3 = 8, ...
+
+-- for part 2, we update the accumulator with the amount of times that card is
+-- represented (c), and then update the future list of counts by repeating the
+-- count for this card x times (the amount that card won)
+
+go :: [Int] -> [Int]
+go = fst . foldl f ([0, 0], repeat 1)
+  where
+    f ([p1, p2], c : cs) x =
+      ( [p1 + if x > 0 then 2 ^ (x - 1) else 0, p2 + c],
+        zipWith (+) cs (replicate x c ++ repeat 0)
+      )
+
+-- parse each card into a number representing the amount of myNumbers which
+-- matched winningNumbers, i.e. the amount that card won
+parseInput :: String -> [Int]
+parseInput = parse' (p `P.sepEndBy` P.newline) id
+  where
+    p = do
+      P.string "Card " `P.between` P.char ':' $ q
+      winningNumbers <- S.fromList <$> P.manyTill (q <* P.char ' ') (P.char '|')
+      myNumbers <- S.fromList <$> q `P.sepBy` P.char ' '
+      pure $ S.size $ S.intersection winningNumbers myNumbers
+    q = P.many P.space *> number
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -2,12 +2,19 @@ module Day04Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
+  \Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
+  \Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n\
+  \Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
+  \Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
+  \Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n\
+  \"
 
 spec :: Spec
 spec = describe "Day 4" $ do
   it "Sample" $ do
-    day04 testInput `shouldBe` []
+    day04 testInput `shouldBe` ["13", "30"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +22,5 @@ spec = describe "Day 4" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day04 actualInput `shouldBe` []
+          day04 actualInput `shouldBe` ["24848", "7258152"]
       )
