fc07ef5cb7041f88c3e0ed0f691f4cd5d9449988
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-05 07:29:05 +0000

    refactor to solve p2 quicker

--- src/Day05.hs
+++ src/Day05.hs
@@ -2,7 +2,8 @@ module Day05 (day05) where
 
 import Common
 import Control.Monad (ap)
-import Debug.Trace (traceM)
+import Data.Function (on)
+import Data.List (elemIndex)
 import Text.Parsec qualified as P
 
 type Input = ([Int], [[(Int, Int, Int)]])
@@ -11,13 +12,18 @@ day05 :: AOCSolution
 day05 = map show . ap [part1, part2] . pure . parseInput
 
 part1 :: Input -> Int
-part1 (s, m) = minimum $ map (`go` m) s
+part1 (s, m) = minimum $ map (go m) s
 
 part2 :: Input -> Int
-part2 = minimum . p2
+part2 (s, m) = minimum $ p2 s
   where
-    p2 ([], _) = []
-    p2 (s1 : s2 : ss, m) = part1 ([s1 .. (s1 + s2 - 1)], m) : p2 (ss, m)
+    p2 [] = []
+    p2 (s1 : s2 : ss) = part1 ([l .. r], m) : p2 ss
+      where
+        seeds = [s1, s1 + floor (sqrt $ fromIntegral s2) .. s1 + s2]
+        locations = map (go m) seeds
+        Just n = elemIndex (minimum locations) locations
+        [l, r] = take 2 $ drop (n - 1) seeds
 
 -- assume maps go a->b, b->c, c->d ...
 parseInput :: String -> Input
@@ -26,11 +32,7 @@ parseInput = (`parse'` id) $ do
   P.newline
   P.newline
   maps <- (`P.sepEndBy` P.newline) $ do
-    P.many P.letter
-    P.string "-to-"
-    P.many P.letter
-    P.string " map:"
-    P.newline
+    P.manyTill P.anyChar (P.string ":\n")
     --
     (`P.sepEndBy` P.newline) $ do
       d <- number <* P.char ' '
@@ -39,12 +41,11 @@ parseInput = (`parse'` id) $ do
       pure (d, s, n)
   pure (seeds, maps)
 
-go :: Int -> [[(Int, Int, Int)]] -> Int
-go i [] = i
-go i (m : ms) = go (f m) ms
+go :: [[(Int, Int, Int)]] -> Int -> Int
+go = flip (foldl f)
   where
-    f [] = i
-    f ((d, s, n) : xs) =
+    f i [] = i
+    f i ((d, s, n) : xs) =
       if i >= s && i < s + n
         then i - s + d
-        else f xs
+        else f i xs
--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -2,7 +2,40 @@ module Day05Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "seeds: 79 14 55 13\n\
+  \\n\
+  \seed-to-soil map:\n\
+  \50 98 2\n\
+  \52 50 48\n\
+  \\n\
+  \soil-to-fertilizer map:\n\
+  \0 15 37\n\
+  \37 52 2\n\
+  \39 0 15\n\
+  \\n\
+  \fertilizer-to-water map:\n\
+  \49 53 8\n\
+  \0 11 42\n\
+  \42 0 7\n\
+  \57 7 4\n\
+  \\n\
+  \water-to-light map:\n\
+  \88 18 7\n\
+  \18 25 70\n\
+  \\n\
+  \light-to-temperature map:\n\
+  \45 77 23\n\
+  \81 45 19\n\
+  \68 64 13\n\
+  \\n\
+  \temperature-to-humidity map:\n\
+  \0 69 1\n\
+  \1 0 69\n\
+  \\n\
+  \humidity-to-location map:\n\
+  \60 56 37\n\
+  \56 93 4"
 
 spec :: Spec
 spec = describe "Day 5" $ do
