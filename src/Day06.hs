c6451e61a3979319dd88c5dc62082f96dede2e09
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-06 09:22:21 +0000

    minor refactor

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,4 +1,4 @@
-module Day06 where -- (day06) where
+module Day06 (day06) where
 
 import Common
 import Text.Parsec qualified as P
@@ -8,14 +8,14 @@ day06 input = map show [product . map solve $ p1, solve p2]
   where
     (p1, p2) = parseInput input
 
-solve :: (Int, Int) -> Int
-solve (fromIntegral -> t, fromIntegral -> d) = maxT - minT + 1
+solve :: (Double, Double) -> Int
+solve (t, d) = maxT - minT + 1
   where
     x = sqrt $ t ^ 2 - 4 * (d + 1)
     maxT = floor ((t + x) / 2)
     minT = ceiling ((t - x) / 2)
 
-parseInput :: String -> ([(Int, Int)], (Int, Int))
+parseInput :: String -> ([(Double, Double)], (Double, Double))
 parseInput = parse $ do
   t <- symbol "Time:" *> P.many (lexeme $ P.many1 P.digit)
   d <- symbol "Distance:" *> P.many (lexeme $ P.many1 P.digit)
