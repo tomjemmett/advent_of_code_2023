364ba9ff7dbc263e5f7cbf4b83dce39c84e1e773
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-06 09:07:09 +0000

    adds day 6 solution

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,6 +1,24 @@
 module Day06 where -- (day06) where
 
 import Common
+import Control.Monad (ap)
+import Data.Bifunctor (bimap)
+import Text.Parsec qualified as P
 
 day06 :: AOCSolution
-day06 input = []
+day06 input = map show [product . map solve $ p1, solve p2]
+  where
+    (p1, p2) = parseInput input
+
+solve :: (Int, Int) -> Int
+solve (fromIntegral -> t, fromIntegral -> d) = maxT - minT + 1
+  where
+    x = sqrt $ t ^ 2 - 4 * (d + 1)
+    maxT = floor ((t + x) / 2)
+    minT = ceiling ((t - x) / 2)
+
+parseInput :: String -> ([(Int, Int)], (Int, Int))
+parseInput = parse $ do
+  t <- symbol "Time:" *> P.many (lexeme $ P.many1 P.digit)
+  d <- symbol "Distance:" *> P.many (lexeme $ P.many1 P.digit)
+  pure (zip (map read t) (map read d), (read $ concat t, read $ concat d))
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -2,12 +2,14 @@ module Day06Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "Time:      7  15   30\n\
+  \Distance:  9  40  200"
 
 spec :: Spec
 spec = describe "Day 6" $ do
   it "Sample" $ do
-    day06 testInput `shouldBe` []
+    day06 testInput `shouldBe` ["288", "71503"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +17,5 @@ spec = describe "Day 6" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day06 actualInput `shouldBe` []
+          day06 actualInput `shouldBe` ["1083852", "23501589"]
       )
