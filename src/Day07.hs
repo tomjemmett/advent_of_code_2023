d8f8dc1dc45afac58392bedfe078eac4569c45e1
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-07 10:40:39 +0000

    adds day 7 solution

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,6 +1,91 @@
-module Day07 where -- (day07) where
+module Day07 (day07) where
 
 import Common
+import Data.Function (on)
+import Data.List (sortBy, group, elemIndex, splitAt)
+import Text.Parsec qualified as P
+
+type Input = [((Hand, [Card]), Int)]
+data Hand
+  = HighCard
+  | OnePair
+  | TwoPair
+  | ThreeOfAKind
+  | FullHouse
+  | FourOfAKind
+  | FiveOfAKind deriving (Show, Eq, Ord)
+data Card
+  = Joker
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace deriving (Show, Eq, Ord)
 
 day07 :: AOCSolution
-day07 input = []
+day07 input = show . solve <$> (parseInput <$> [False, True] <*> pure input)
+
+solve :: Input -> Int
+solve = sum . zipWith (*) [1..] . map snd
+
+parseInput :: Bool -> String -> Input
+parseInput joker = sortBy (compare `on` fst) . parseLinesWith do
+  cs <- parseCards joker <$> P.many (P.oneOf "23456789TJQKA")
+  bid <- P.char ' ' >> number
+  pure ((handType cs, cs), bid)
+
+parseCards :: Bool -> String -> [Card]
+parseCards joker = map parseCard
+  where
+    parseCard = \case
+      '2' -> Two
+      '3' -> Three
+      '4' -> Four
+      '5' -> Five
+      '6' -> Six
+      '7' -> Seven
+      '8' -> Eight
+      '9' -> Nine
+      'T' -> Ten
+      'J' -> if joker then Joker else Jack
+      'Q' -> Queen
+      'K' -> King
+      'A' -> Ace 
+
+handType :: [Card] -> Hand
+handType xs = case length xs' of
+  1 -> FiveOfAKind
+  2 -> if fst (head xs') == 4 
+    then FourOfAKind
+    else FullHouse
+  3 -> if fst (head xs') == 3
+    then ThreeOfAKind
+    else TwoPair
+  4 -> OnePair
+  5 -> HighCard
+  where
+    xs' = replaceJokers . countCards $ xs
+    countCards :: [Card] -> [(Int, Card)]
+    countCards = sortBy (flip compare `on` fst) .
+          map (\xs@(x:_) -> (length xs, x)) .
+          group .
+          sortBy (flip compare)
+    replaceJokers :: [(Int, Card)] -> [(Int, Card)]
+    replaceJokers xs 
+      | length xs == 1 = xs
+      | otherwise = case Joker `elemIndex` map snd xs of
+        Nothing -> xs
+        Just i ->
+          let
+            (a, (j, _):b) = splitAt i xs
+            ((c, d):e) = a ++ b
+          in 
+            (c+j, d):e
--- test/Day07Spec.hs
+++ test/Day07Spec.hs
@@ -2,12 +2,18 @@ module Day07Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "32T3K 765\n\
+  \T55J5 684\n\
+  \KK677 28\n\
+  \KTJJT 220\n\
+  \QQQJA 483"
+
 
 spec :: Spec
 spec = describe "Day 7" $ do
   it "Sample" $ do
-    day07 testInput `shouldBe` []
+    day07 testInput `shouldBe` ["6440", "5905"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +21,5 @@ spec = describe "Day 7" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day07 actualInput `shouldBe` []
+          day07 actualInput `shouldBe` ["252656917", "253499763"]
       )
