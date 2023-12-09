05dabd1d6f84c0bd1f2f7b3655ff16c4658051be
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-09 08:34:42 +0000

    adds day 9 solution

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,6 +1,25 @@
-module Day09 where -- (day09) where
+module Day09 (day09) where
 
 import Common
+import Control.Monad (ap)
+import Text.Parsec qualified as P
+
+type Input = [[Int]]
 
 day09 :: AOCSolution
-day09 input = []
+day09 = map show . ap [part1, part2] . pure . parseInput
+
+part1 :: Input -> Int
+part1 = sum . map go
+part2 :: Input -> Int
+part2 = part1 . map reverse
+
+go :: [Int] -> Int
+go x = last x + if all (== d) x' 
+  then d 
+  else go x'
+  where
+    x'@(d:_) = zipWith (-) (drop 1 x) x
+
+parseInput :: String -> Input
+parseInput = parseLinesWith (P.many number)
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -2,12 +2,15 @@ module Day09Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "0 3 6 9 12 15\n\
+  \1 3 6 10 15 21\n\
+  \10 13 16 21 30 45"
 
 spec :: Spec
 spec = describe "Day 9" $ do
   it "Sample" $ do
-    day09 testInput `shouldBe` []
+    day09 testInput `shouldBe` ["114", "2"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +18,5 @@ spec = describe "Day 9" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day09 actualInput `shouldBe` []
+          day09 actualInput `shouldBe` ["2005352194", "1077"]
       )
