4f2223cdea55b1cbd35db5ec6da9049a46816d77
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-11 16:38:54 +0000

    minor refactors

--- src/Day11.hs
+++ src/Day11.hs
@@ -9,27 +9,29 @@ import Data.List (nub, tails, transpose)
 type Expansion = Int -> M.HashMap Int Int
 
 day11 :: AOCSolution
-day11 = map show . ap [go 1, go 999999] . pure . parseInput
+day11 = map show . ap (go <$> [1, 999999]) . pure . parseInput
 
 go :: Int -> ([Point2d], Expansion, Expansion) -> Int
-go n (i, rows, cols) = sum [ manhattanDistance x y | (x:ys) <- tails (nub i'), y <- ys]
+go n (i, rows, cols) = sum [manhattanDistance x y | (x : ys) <- tails (nub i'), y <- ys]
   where
     r = rows n
     c = cols n
-    i' =  map (bimap (c M.!) (r M.!)) i
+    i' = map (bimap (c M.!) (r M.!)) i
 
 parseInput :: String -> ([Point2d], Expansion, Expansion)
-parseInput input = (i, M.fromList . expand 0 0 ls, M.fromList . expand 0 0 (transpose ls))
+parseInput input = (i, e ls, e (transpose ls))
   where
     ls = lines input
-    i = [ (c, r)
-        | (r, l) <- zip [0..] ls
-        , (c, v) <- zip [0..] l
-        , v == '#'
-        ]
+    e = M.fromList ... expand 0 0
+    i =
+      [ (c, r)
+        | (r, l) <- zip [0 ..] ls,
+          (c, v) <- zip [0 ..] l,
+          v == '#'
+      ]
 
 expand :: Int -> Int -> [String] -> Int -> [(Int, Int)]
 expand _ _ [] _ = []
-expand a i (x:xs) n = (i, i + a') : expand a' (succ i) xs n
+expand a i (x : xs) n = (i, i + a') : expand a' (succ i) xs n
   where
-    a' = a + (if all (=='.') x then n else 0)
+    a' = a + (if all (== '.') x then n else 0)
