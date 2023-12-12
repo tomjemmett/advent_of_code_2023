5b49cc42d4c1a0aff7bacc209f143e06fd255f19
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-12 08:05:25 +0000

    adds day 12 solution

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,6 +1,49 @@
-module Day12 where -- (day12) where
+module Day12 (day12) where
 
 import Common
+import Control.Monad (ap)
+import Data.Array qualified as A
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
 
 day12 :: AOCSolution
-day12 input = []
+day12 = map show . ap [part1, part2] . pure . parseInput
+
+part1, part2 :: [(String, [Int])] -> Int
+part1 = sum . map go
+part2 = part1 . map unfold
+  where
+    unfold :: (String, [Int]) -> (String, [Int])
+    unfold (xs, ys) = (intercalate "?" (replicate 5 xs), concat $ replicate 5 ys)
+
+parseInput :: String -> [(String, [Int])]
+parseInput = map ((\[a, b] -> (a, commaSeparatedInts b)) . splitOn " ") . lines
+
+go :: (String, [Int]) -> Int
+go (conditionRecord, groupSizes) = valids conditionRecord groupSizes
+  where
+    lcr = length conditionRecord
+    lgs = length groupSizes
+
+    v =
+      A.array
+        ((0, 0), (lcr, lgs))
+        [ ((x, y), f (drop x conditionRecord) (drop y groupSizes))
+          | x <- [0 .. lcr],
+            y <- [0 .. lgs]
+        ]
+
+    valids :: [Char] -> [Int] -> Int
+    valids cr gs = v A.! (lcr - length cr, lgs - length gs)
+
+    f :: [Char] -> [Int] -> Int
+    f [] [] = 1
+    f [] _ = 0
+    f ('#' : _) [] = 0
+    f ('.' : xs) ys = valids xs ys
+    f xs@('#' : _) (y : ys)
+      | length xs < y = 0
+      | '.' `elem` take y xs = 0
+      | length xs > y && xs !! y == '#' = 0
+      | otherwise = valids (drop (y + 1) xs) ys
+    f ('?' : xs) ys = valids xs ys + f ('#' : xs) ys
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -2,12 +2,18 @@ module Day12Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "???.### 1,1,3\n\
+  \.??..??...?##. 1,1,3\n\
+  \?#?#?#?#?#?#?#? 1,3,1,6\n\
+  \????.#...#... 4,1,1\n\
+  \????.######..#####. 1,6,5\n\
+  \?###???????? 3,2,1"
 
 spec :: Spec
 spec = describe "Day 12" $ do
   it "Sample" $ do
-    day12 testInput `shouldBe` []
+    day12 testInput `shouldBe` ["21", "525152"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +21,5 @@ spec = describe "Day 12" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day12 actualInput `shouldBe` []
+          day12 actualInput `shouldBe` ["7716", "18716325559999"]
       )
