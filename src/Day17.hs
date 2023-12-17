298fa23490dfe39995bc1dd5c9d78e7f74d129eb
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-17 23:31:41 +0000

    adds day 17 solution

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,6 +1,65 @@
-module Day17 where -- (day17) where
+module Day17 (day17) where
 
 import Common
+import Control.Monad (ap, guard)
+import Data.Array qualified as A
+import Data.Char (digitToInt)
+import PathFinding
+
+type Input = A.Array Point2d Int
+
+type Movement = (Point2d, Char)
 
 day17 :: AOCSolution
-day17 input = []
+day17 = map show . ap (go <$> [(1, 3), (4, 10)]) . pure . parseInput
+
+go :: (Int, Int) -> Input -> Int
+go n i = fst . last $ unsafeFindPath (move t n i) ((0, 0), 'I') (t, 'G')
+  where
+    (_, t) = A.bounds i
+
+move :: Point2d -> (Int, Int) -> Input -> Movement -> [(Int, Movement)]
+move target (minDist, maxDist) i (p@(x, y), d) =
+  if p == target
+    then [(0, (p, 'G'))]
+    else moves
+  where
+    inRange = A.inRange (A.bounds i)
+    moves = case d of
+      'I' -> moveS ++ moveE
+      'N' -> moveE ++ moveW
+      'S' -> moveE ++ moveW
+      'E' -> moveN ++ moveS
+      'W' -> moveN ++ moveS
+    moveN = do
+      y' <- [minDist .. maxDist]
+      let p' = (x, y - y')
+      guard $ inRange p'
+      let v = sum [i A.! (x, y - y'') | y'' <- [1 .. y']]
+      pure (v, (p', 'N'))
+    moveS = do
+      y' <- [minDist .. maxDist]
+      let p' = (x, y + y')
+      guard $ inRange p'
+      let v = sum [i A.! (x, y + y'') | y'' <- [1 .. y']]
+      pure (v, (p', 'S'))
+    moveE = do
+      x' <- [minDist .. maxDist]
+      let p' = (x + x', y)
+      guard $ inRange p'
+      let v = sum [i A.! (x + x'', y) | x'' <- [1 .. x']]
+      pure (v, (p', 'E'))
+    moveW = do
+      x' <- [minDist .. maxDist]
+      let p' = (x - x', y)
+      guard $ inRange p'
+      let v = sum [i A.! (x - x'', y) | x'' <- [1 .. x']]
+      pure (v, (p', 'E'))
+
+parseInput :: String -> Input
+parseInput input = A.array ((0, 0), (cn - 1, rn - 1)) g
+  where
+    i = lines input
+    rn = length i
+    cn = length $ head i
+    g = [((c, r), digitToInt v) | (r, vs) <- zip [0 ..] i, (c, v) <- zip [0 ..] vs]
--- test/Day17Spec.hs
+++ test/Day17Spec.hs
@@ -2,12 +2,25 @@ module Day17Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "2413432311323\n\
+  \3215453535623\n\
+  \3255245654254\n\
+  \3446585845452\n\
+  \4546657867536\n\
+  \1438598798454\n\
+  \4457876987766\n\
+  \3637877979653\n\
+  \4654967986887\n\
+  \4564679986453\n\
+  \1224686865563\n\
+  \2546548887735\n\
+  \4322674655533"
 
 spec :: Spec
 spec = describe "Day 17" $ do
   it "Sample" $ do
-    day17 testInput `shouldBe` []
+    day17 testInput `shouldBe` ["102", "94"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +28,5 @@ spec = describe "Day 17" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day17 actualInput `shouldBe` []
+          day17 actualInput `shouldBe` ["956", "1106"]
       )
