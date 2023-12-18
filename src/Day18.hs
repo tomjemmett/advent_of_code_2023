8aa89563409838d7f03e026ed3626c9961d73425
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-18 11:20:10 +0000

    adds day 18 solution

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,6 +1,63 @@
-module Day18 where -- (day18) where
+module Day18 (day18) where
 
 import Common
+import Control.Monad (ap)
+import Text.Parsec qualified as P
+
+type Input = [(Char, Int)]
 
 day18 :: AOCSolution
-day18 input = []
+day18 = map show . ap [part1, part2] . pure . parseInput
+
+part1, part2 :: (Input, Input) -> Int
+part1 = area . fst
+part2 = area . snd
+
+area :: Input -> Int
+area i = picks e l + l
+  where
+    e = getEdge i
+    l = getLength i
+
+-- https://en.wikipedia.org/wiki/Pick%27s_theorem
+picks :: [Point2d] -> Int -> Int
+picks edges len = shoelace edges + 1 - (len `div` 2)
+
+-- https://en.wikipedia.org/wiki/Shoelace_formula
+shoelace :: [Point2d] -> Int
+shoelace xs = (`div` 2) . abs $ f (last xs : xs)
+  where
+    f [_] = 0
+    f ((x1, y1) : p@(x2, y2) : xs) = x1 * y2 - x2 * y1 + f (p : xs)
+
+getLength :: Input -> Int
+getLength = foldr ((+) . snd) 0
+
+getEdge :: Input -> [Point2d]
+getEdge = fst . foldl g ([], (0, 0))
+  where
+    g :: ([Point2d], Point2d) -> (Char, Int) -> ([Point2d], Point2d)
+    g (m, p@(x, y)) (d, n) = (p' : m, p')
+      where
+        p' = case d of
+          'L' -> (x - n, y)
+          'R' -> (x + n, y)
+          'U' -> (x, y - n)
+          'D' -> (x, y + n)
+
+parseInput :: String -> (Input, Input)
+parseInput =
+  unzip . parseLinesWith do
+    d <- lexeme $ P.oneOf "LRUD"
+    n <- number
+    c <- (P.char '(' `P.between` P.char ')') do
+      P.char '#'
+      a <- P.count 5 P.alphaNum
+      b <- P.digit
+      let d = case b of
+            '0' -> 'R'
+            '1' -> 'D'
+            '2' -> 'L'
+            '3' -> 'U'
+      pure (d, read $ "0x" ++ a)
+    pure ((d, n), c)
--- test/Day18Spec.hs
+++ test/Day18Spec.hs
@@ -2,12 +2,26 @@ module Day18Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "R 6 (#70c710)\n\
+  \D 5 (#0dc571)\n\
+  \L 2 (#5713f0)\n\
+  \D 2 (#d2c081)\n\
+  \R 2 (#59c680)\n\
+  \D 2 (#411b91)\n\
+  \L 5 (#8ceee2)\n\
+  \U 2 (#caa173)\n\
+  \L 1 (#1b58a2)\n\
+  \U 2 (#caa171)\n\
+  \R 2 (#7807d2)\n\
+  \U 3 (#a77fa3)\n\
+  \L 2 (#015232)\n\
+  \U 2 (#7a21e3)"
 
 spec :: Spec
 spec = describe "Day 18" $ do
   it "Sample" $ do
-    day18 testInput `shouldBe` []
+    day18 testInput `shouldBe` ["62", "952408144115"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +29,5 @@ spec = describe "Day 18" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day18 actualInput `shouldBe` []
+          day18 actualInput `shouldBe` ["38188", "93325849869340"]
       )
