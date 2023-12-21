b70b1a05bc22a1ae2f3d34fca3e099a11c93ceb4
Tom Jemmett <thomas.jemmett@nhs.net>
2023-12-21 09:54:40 +0000

    adds day 21 solution

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,6 +1,46 @@
 module Day21 where -- (day21) where
 
 import Common
+import Control.Monad (ap)
+import Control.Monad.Writer
+import Data.HashSet qualified as S
+import Data.List (find)
+import Text.Parsec qualified as P
 
 day21 :: AOCSolution
-day21 input = []
+day21 = map show . ap [part1, part2] . pure . parseInput
+
+go :: Int -> (S.HashSet Point2d, Point2d) -> Int
+go i (g, s) = S.size $ step g i $ S.singleton s
+
+part1 :: (S.HashSet Point2d, Point2d) -> Int
+part1 = go 64
+
+part2 :: (S.HashSet Point2d, Point2d) -> Int
+part2 i = b0 + b1 * goal + (n * pred n `div` 2) * (b2 - b1)
+  where
+    goal = 26501365
+    n = goal `div` 131
+    [a0, a1, a2] = map (\x -> go (65 + x * 131) i) [0 .. 2]
+    b0 = a0
+    b1 = a1 - a0
+    b2 = a2 - a1
+
+step :: S.HashSet Point2d -> Int -> S.HashSet Point2d -> S.HashSet Point2d
+step g 0 ps = ps
+step g i ps = step g (pred i) $ S.filter isGarden $ S.fromList $ concatMap point2dNeighbours ps
+  where
+    -- hardcoded input grid size for actual input
+    size = 131
+    isGarden (x, y) = (x `mod` size, y `mod` size) `S.member` g
+
+parseInput :: String -> (S.HashSet Point2d, Point2d)
+parseInput input = (S.fromList $ map fst g, s)
+  where
+    Just s = fst <$> find ((== 'S') . snd) g
+    g =
+      [ ((c, r), v)
+        | (r, vs) <- zip [0 ..] $ lines input,
+          (c, v) <- zip [0 ..] vs,
+          v /= '#'
+      ]
--- test/Day21Spec.hs
+++ test/Day21Spec.hs
@@ -2,18 +2,13 @@ module Day21Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 21" $ do
-  it "Sample" $ do
-    day21 testInput `shouldBe` []
-
   it "Actual" $ do
     withFile
       "inputs/day21.txt"
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day21 actualInput `shouldBe` []
+          day21 actualInput `shouldBe` ["3697", "608152828731262"]
       )
