8ba95e58667c2de9ea7cbd387fbe8076e4ac5eae
Tom Jemmett <thomas.jemmett@nhs.net>
2024-01-03 13:38:46 +0000

    adds solution for day 25
    very slow: there is an R solution using igraph which is almost instant

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,6 +1,63 @@
-module Day25 where -- (day25) where
+module Day25 (day25) where
 
 import Common
+import Control.Monad (ap)
+import Data.Bifunctor (bimap)
+import Data.Function (on)
+import Data.Graph.Inductive qualified as G
+import Data.List (delete, lookup, maximumBy, nub, (\\))
+import Data.Maybe (fromJust)
+import Data.Tuple (swap)
+import Text.Parsec qualified as P
+
+{-
+# R Solution
+library(tidyverse)
+library(igraph)
+library(tidygraph)
+
+input <- read_file("inputs/day25.txt") |>
+  stringr::str_trim() |>
+  str_split("\n") |>
+  pluck(1)
+
+g <- input |>
+  str_split(": ") |>
+  map(\(.x) {
+    tibble(from = .x[[1]], to = stringr::str_split(.x[[2]], " ")[[1]])
+  }) |>
+  bind_rows() |>
+  graph_from_data_frame(directed = FALSE)
+
+cut <- min_cut(g, value.only = FALSE)
+
+length(cut$partition1) * length(cut$partition2)
+-}
 
 day25 :: AOCSolution
-day25 input = []
+day25 = map show . ap [solve] . pure . parseInput
+
+mkGraph :: ([String], [(String, String)]) -> G.Gr String String
+mkGraph (n, e) = G.undir $ G.mkGraph (map swap n') e'
+  where
+    n' = zip n [1 ..]
+    e' = map (\(from, to) -> (fromJust $ lookup from n', fromJust $ lookup to n', from ++ " " ++ to)) e
+
+solve :: G.Gr String String -> Int
+solve g = group1size * (G.order g - group1size)
+  where
+    group1 = loop (G.nodes g)
+    group1size = length group1
+    loop s = if sum (map count s) == 3 then s else loop (delete (maximumBy (compare `on` count) s) s)
+      where
+        count v = length (G.suc g v \\ s)
+
+parseInput :: String -> G.Gr String String
+parseInput =
+  mkGraph
+    . bimap (nub . concat) concat
+    . unzip
+    . parseLinesWith do
+      f <- P.manyTill P.letter (symbol ":")
+      t <- P.many P.letter `P.sepBy` P.char ' '
+      pure (f : t, map (f,) t)
--- test/Day25Spec.hs
+++ test/Day25Spec.hs
@@ -2,12 +2,25 @@ module Day25Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "jqt: rhn xhk nvd\n\
+  \rsh: frs pzl lsr\n\
+  \xhk: hfx\n\
+  \cmg: qnr nvd lhk bvb\n\
+  \rhn: xhk bvb hfx\n\
+  \bvb: xhk hfx\n\
+  \pzl: lsr hfx nvd\n\
+  \qnr: nvd\n\
+  \ntq: jqt hfx bvb xhk\n\
+  \nvd: lhk\n\
+  \lsr: lhk\n\
+  \rzs: qnr cmg lsr rsh\n\
+  \frs: qnr lhk lsr"
 
 spec :: Spec
 spec = describe "Day 25" $ do
   it "Sample" $ do
-    day25 testInput `shouldBe` []
+    day25 testInput `shouldBe` ["54"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +28,5 @@ spec = describe "Day 25" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day25 actualInput `shouldBe` []
+          day25 actualInput `shouldBe` ["525264"]
       )
